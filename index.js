cdbee786c46d37ef878fdc6b04546a7c29def5c9
himanshu-kr-2000 <85312318+himanshu-kr-2000@users.noreply.github.com>
2021-06-04 15:13:02 +0530

    Fetched individual data and separated js from html

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
-
+var app = angular.module('employee', []);
+app.controller('myfunc', function($scope, $http) {
+    $http.get("https://jsonplaceholder.typicode.com/users")
+    .then(function(response) {
+        var obj = response.data;
+        $scope.rec = obj;
+    });
+     $scope.red = function(ind){
+        console.log(ind);
+        ind += 1;
+        $http.get("https://jsonplaceholder.typicode.com/users/" + ind)
+        .then(function(response) {
+            var obj = response.data;
+            $scope.details = obj;
+            console.log(obj);
+        });
+     }
+})
